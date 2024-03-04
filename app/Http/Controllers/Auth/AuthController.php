7ab073d965f77d9ca7f596e57a751a51896ae834
FarrukhShomurodov <uamerike@gmail.com>
2024-03-04 16:34:05 +0500

    chech request

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
      */
     public function login(Request $request): JsonResponse
     {
-        return new JsonResponse(redirect()->intended()->getCallback());
+        return new JsonResponse( $request->headers->get('referer'));
         //validate
         $validated = $request->validate([
             'phone_number' => 'required|regex:/^\+?[0-9]{10,}$/',
