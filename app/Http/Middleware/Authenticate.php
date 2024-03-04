c964261025b0d366b794af07f86e0ed25df102d7
FarrukhShomurodov <uamerike@gmail.com>
2024-03-05 03:23:58 +0500

    test redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,24 +13,24 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            // Start modified line
-            if ($request->path() === 'oauth/authorize') {
-                if (isset($request->query()['client_id'])) {
-                    $params = [
-                        'client_id' => $request->query('client_id'),
-                        'return_to' => $request->getRequestUri(),
-                    ];
-
-                    $queryString = http_build_query($params);
-
-                    return redirect()->to('api/login?' . $queryString);
-                } else {
-                    return route('login');
-                }
-            }
-            // End modified line
-            return route('login');
-        }
+//        if (! $request->expectsJson()) {
+//            // Start modified line
+//            if ($request->path() === 'oauth/authorize') {
+//                if (isset($request->query()['client_id'])) {
+//                    $params = [
+//                        'client_id' => $request->query('client_id'),
+//                        'return_to' => $request->getRequestUri(),
+//                    ];
+//
+//                    $queryString = http_build_query($params);
+//
+//                    return redirect()->to('api/login?' . $queryString);
+//                } else {
+//                    return route('login');
+//                }
+//            }
+//            // End modified line
+//            return route('login');
+//        }
     }
 }
