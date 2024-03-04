aa75779d4bee36d3e19d4260c6f65410ec7bf6c1
FarrukhShomurodov <uamerike@gmail.com>
2024-03-05 04:03:17 +0500

    test redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Session;
 
 class Authenticate extends Middleware
 {
@@ -20,9 +21,9 @@ class Authenticate extends Middleware
                         'return_to' => $request->getRequestUri(),
                     ];
 
-                    $queryString = http_build_query($params);
+                    Session::put('redirect_data', $params);
 
-                    return redirect()->to('api/login?' . $queryString);
+                    return redirect()->to('api/login');
                 } else {
                     return route('login');
                 }
