237701daeaa2bbf0723f0ed5caf482480f78c139
FarrukhShomurodov <uamerike@gmail.com>
2024-03-05 13:17:32 +0500

    test redirect

--- app/Http/Middleware/SaveRedirectUrl.php
+++ app/Http/Middleware/SaveRedirectUrl.php
@@ -17,6 +17,7 @@ class SaveRedirectUrl
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->path() === 'oauth/authorize') {
+            dd($request->getRequestUri());
             Session::put('redirect_data', $request->getRequestUri());
         }
 
